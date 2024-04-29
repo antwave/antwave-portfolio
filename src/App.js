732bdb1f7cae329817ff599cf7dfc87fce15b166
Antonio <antw4ve@icloud.com>
2024-04-29 19:15:32 +0200

    added about section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Skills from "./components/Skills";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <About />
       <Skills />
     </div>
   );
---
+++ src/components/About.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import aboutImg from "../assets/about.jpg";
+
+const About = () => {
+  return (
+    <div className="text-white max-w-[1200px] mx-auto my-12" id="about">
+      <div className="md:grid md:grid-cols-2 sm:py-16">
+        <div className="mt-4 md:mt-0 text-left flex">
+          <div className="my-auto mx-6">
+            <h2 className="text-4xl font-bold mb-4 primary-color">About Me</h2>
+            <p className="text-base lg:text-lg">
+              Una absoluta barbaridad de texto contando lo que soy y lo que hago
+              que voy a rellenar en un ratín de momento sigo escribidendo porque
+              blablablabalbalblakjsdñlkfjasñldkjfañlskdjf
+            </p>
+          </div>
+        </div>
+        <img
+          className="mx-auto rounded-3xl py-8 md:py-0"
+          src={aboutImg}
+          width={300}
+          height={300}
+          alt="antwave"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default About;
